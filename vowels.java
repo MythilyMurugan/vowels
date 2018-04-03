a2d3ba80f7ff09d40518beee9aecabdb465cec74
MythilyMurugan <34851769+MythilyMurugan@users.noreply.github.com>
2018-04-03 12:09:03 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class findvowel
+{
+    public static void main(String Args[])
+    {
+        System.out.println("Enter a string");
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println("The vowels are :-");
+        vowel(s);
+    }
+    
+    public static void vowel(String st)
+    {
+        int i;
+        for(i=0;i<st.length();i++)
+        {
+            if((st.charAt(i)=='a')||(st.charAt(i)=='u')||(st.charAt(i)=='o')||(st.charAt(i)=='i')||(st.charAt(i)=='e'))
+            {
+                System.out.println(st.charAt(i));
+            }
+        }
+    }
+}
